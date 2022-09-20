822f7d2c7a4bbf1beaa3d83be6a76bc37b9bb63f
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-20 15:29:44 -0600

    creating a collapsing nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,33 +8,62 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-
   return (
-    <nav className="navbar">
-      
-        <Link exact="true" className="homepage" to="/">
-          <h1>Lindsey Lansford</h1>
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <Link exact="true" className="navbar-brand mb-0 h1" to="/">
+          Lindsey Lansford
         </Link>
-      <div className="links">
-        <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")}
-          to="/about"> About
-          {/* <FontAwesomeIcon icon={faCircleUser}/> */}
-        </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="portfolio">Portfolio
-          {/* <FontAwesomeIcon icon={faFolderOpen} /> */}
-        </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")}
-          to="/resume">Resume
-          {/* <FontAwesomeIcon icon={faFolderOpen} /> */}
-        </NavLink>
-        <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")}
-          to="/contact">Contact
-          {/* <FontAwesomeIcon icon={faAddressCard} /> */}
-        </NavLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav offset-md-6 ms-md-auto ">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+              to="portfolio"
+            >
+              Portfolio
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+              to="/resume"
+            >
+              Resume
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </div>
+        </div>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
