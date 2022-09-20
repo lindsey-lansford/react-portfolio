e6556fe3fbcbffd0930f222e1613603680f5f528
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-20 16:57:33 -0600

    adding templace for resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,6 +4,19 @@ const Resume = () => {
   return (
     <div className="resume-page-container">
       <h1 className="title">Resume</h1>
+      <div class="card text-center">
+  <div class="card-header">
+    {/* Download my Resume */}
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Let's Work Together!</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Download Here</a>
+  </div>
+  <div class="card-footer text-muted">
+
+  </div>
+</div>
     </div>
   )
 }
