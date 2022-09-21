ff8acecb11d7d99f2ef92d18e7e5c26708d0f5bd
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-20 18:04:33 -0600

    adding project data & images

---
+++ src/data/projects.js
@@ -0,0 +1,54 @@
+import HabitStack from "../images/habitstack.png";
+
+const projects = {
+  
+  habitStack: {
+    title: "Habit Stack",
+    image: HabitStack,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+    prod: "https://habitstack.herokuapp.com/",
+    repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+  },
+  
+  concertConcierge: {
+    title: "Concert Concierge",
+    image: HabitStack,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+    prod: "https://habitstack.herokuapp.com/",
+    repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+  },
+  
+  weather: {
+    title: "Weather Dashboard",
+    image: HabitStack,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+    prod: "https://habitstack.herokuapp.com/",
+    repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+  },
+
+  projectFour: {
+    title: "Project name 4",
+    image: HabitStack,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+    prod: "https://habitstack.herokuapp.com/",
+    repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+  },
+
+  projectFive: {
+    title: "Project name 5",
+    image: HabitStack,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+    prod: "https://habitstack.herokuapp.com/",
+    repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+  },
+
+  projectSix: {
+    title: "Project name 6",
+    image: HabitStack,
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+    prod: "https://habitstack.herokuapp.com/",
+    repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+  },
+};
+
+export default projects;
