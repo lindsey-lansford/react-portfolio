a524d53f07784162a23dda09d09c12a182c7a69c
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-20 18:22:50 -0600

    got the cards working!!

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,23 +1,19 @@
-// import { Link } from "react-router-dom";
-
 const ProjectCard = (props) => {
   
-  const project = props.project;
-
   return (  
-    <div className="card">
+    <div className={`card ${props.id}`}>
       <div className="card-header">
-        Project Title 
+        {props.title}
       </div>
-      <img src="" alt="" className="project-photo" />
+      <img src={props.image} alt={props.title} className="project-photo" />
       <div className="card-body">
         <div className="card-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem perspiciatis dolore laboriosam quasi ad, quidem velit doloribus, magni veniam earum fugiat nihil, iste ratione delectus voluptas eum impedit. Quae, commodi?
+        {props.description}
         </div>
         <div className="card-body">
-        <a href="#" target="_blank"
-              rel="noreferrer" className="card-link">Project Link</a>
-        <a href="#" target="_blank"
+        <a href={props.prod} target="_blank"
+              rel="noreferrer" className="card-link">Production Link</a>
+        <a href={props.repo} target="_blank"
               rel="noreferrer" className="card-link">GitHub Repo</a>
         </div>
       </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,18 +1,82 @@
 import ProjectCard from '../components/ProjectCard';
 
+
+import HabitStack from "../images/habitstack.png";
+
+
 const Portfolio = () => {
-  // const [portfolio, setPortfolio] = useState([]);
+  
+  const projects = [
+    {
+      title: "Habit Stack",
+      image: HabitStack,
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+      prod: "https://habitstack.herokuapp.com/",
+      repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+      id: 1,
+    },
+    
+    {
+      title: "Concert Concierge",
+      image: HabitStack,
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+      prod: "https://habitstack.herokuapp.com/",
+      repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+      id: 2,
+    },
+    
+    {
+      title: "Weather Dashboard",
+      image: HabitStack,
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+      prod: "https://habitstack.herokuapp.com/",
+      repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+      id: 3,
+    },
+    
+    {
+      title: "Project name 4",
+      image: HabitStack,
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+      prod: "https://habitstack.herokuapp.com/",
+      repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+      id: 4,
+    },
+  
+    {
+      title: "Project name 5",
+      image: HabitStack,
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+      prod: "https://habitstack.herokuapp.com/",
+      repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+      id: 5,
+    },
+  
+    {
+      title: "Project name 6",
+      image: HabitStack,
+      description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde delectus in dolorem soluta hic maiores eos earum faPlaceOfWorship.",
+      prod: "https://habitstack.herokuapp.com/",
+      repo: "https://github.com/lindsey-lansford/habit-map/tree/main",
+      id: 6,
+    },
+  ];
 
   return (
     <div className="portfolio-page-container">
       <h1 className="title">Portfolio</h1>
       <div className="container">
-    <ProjectCard>/</ProjectCard>
-    <ProjectCard>/</ProjectCard>
-    <ProjectCard>/</ProjectCard>
-    <ProjectCard>/</ProjectCard>
-    <ProjectCard>/</ProjectCard>
-    <ProjectCard>/</ProjectCard>
+        {projects.map((project) => (
+          <ProjectCard
+            title={project.name}
+            image={project.image}
+            description={project.description}
+            repo={project.repo}
+            deployed={project.prod}
+            id={project.id}
+            key={project.id}
+            />
+          ))}
       </div>
     </div>
   )
