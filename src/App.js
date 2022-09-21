1100a0ee57dd92c5cd1f399d99a9c44bc7061e9a
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-21 11:05:53 -0600

    editing homepage link

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Navbar />
         <div className="content">
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route path="/react-portfolio/" element={<Home />}/>
           <Route path="/about" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/resume" element={<Resume />} />
