2edd295f258f8d17b9bb1f1fa7d82b55ede0f379
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-21 14:21:38 -0600

    trying to fix hp err

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Navbar />
         <div className="content">
         <Routes>
-          <Route path="/react-portfolio" element={<Home />}/>
+          <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/resume" element={<Resume />} />
