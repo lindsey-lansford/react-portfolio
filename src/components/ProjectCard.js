fe9069b50c74afcfd79c8121a9aa84cbbb147be5
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-10-20 16:37:53 -0600

    adding id to cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,26 +1,35 @@
-import "../styles/projectCard.css"
+import "../styles/projectCard.css";
 
 const ProjectCard = (props) => {
-  
-  return (  
-    <div className={`card text-center ${props.id}`}>
-      <div className="card-header">
-        {props.title}
-      </div>
-      <img src={props.image} alt={props.title} className="img-fluid" />
-      <div className="card-body">
-        <div className="card-text">
-        {props.description}
-        </div>
+  return (
+    <div className="container" id="portfolio-card">
+      <div className={`card text-center ${props.id}`}>
+        <div className="card-header">{props.title}</div>
+        <img src={props.image} alt={props.title} className="img-fluid" />
         <div className="card-body">
-        <a href={props.prod} target="_blank"
-              rel="noreferrer" className="card-link">Production Link</a>
-        <a href={props.repo} target="_blank"
-              rel="noreferrer" className="card-link">GitHub Repo</a>
+          <div className="card-text">{props.description}</div>
+          <div className="card-body">
+            <a
+              href={props.prod}
+              target="_blank"
+              rel="noreferrer"
+              className="card-link"
+            >
+              Production Link
+            </a>
+            <a
+              href={props.repo}
+              target="_blank"
+              rel="noreferrer"
+              className="card-link"
+            >
+              GitHub Repo
+            </a>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default ProjectCard; 
+export default ProjectCard;
