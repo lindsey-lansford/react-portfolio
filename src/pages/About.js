3bcb35476a373308728b63a1be9aa6becd0756bc
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-10-25 16:35:33 -0600

    getting the layout closer

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,42 +1,48 @@
 import * as React from 'react';
-// import "../styles/about.css";
+import "../styles/about.css";
 import bioImage from "../images/profpic2.jpg";
 import { Container, Stack, Paper, Avatar, Box } from '@mui/material';
-// import { styled } from '@mui/material/styles';
 
 
 const About = () => {
 
-  // const Item = styled(Paper)(({ theme }) => ({
-  //   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  //   ...theme.typography.body2,
-  //   padding: theme.spacing(1),
-  //   textAlign: 'center',
-  //   color: theme.palette.text.secondary,
-  // }));
-
-  return (
-    <div className="about-page-container">
+return (
+  <div className="about-page-container">
     <h1 className="title">About Me</h1>
     <Container maxWidth="lg">
       <Stack
-        direction={{ xs: 'column', sm: 'row' }}
-        spacing={{ xs: 1, sm: 2, md: 4 }}
+        direction="row"
+        spacing={{ xs: 1, sm: 2, md: 4, lg: 5 }}
+        alignItems="center"
         >
-          <Avatar src={bioImage} alt="profile" sx={{ width: 150, height: 210}}/>
-        {/* <Item><img src={bioImage} alt="profile" className="profile-image" /></Item> */}
+          <Avatar src={bioImage} alt="profile" sx={{ width: 150, height: 210, margin: 2}}/>
         <Box>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
             delectus ullam distinctio quod consequuntur iste inventore magnam
             optio!
-          </Box>
-          <Box>
+        </Box>
+      </Stack>
+      <Stack
+      direction="row"
+      spacing={{ xs: 1, sm: 2, md: 4 }}
+      >
+        <Box>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
             quod pariatur facilis eos sed sint tenetur nisi iste. Dolor eos
             temporibus facere.
-            </Box>
+        </Box>
+        <Box>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            quod pariatur facilis eos sed sint tenetur nisi iste. Dolor eos
+            temporibus facere.
+        </Box>
+        <Box>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            quod pariatur facilis eos sed sint tenetur nisi iste. Dolor eos
+            temporibus facere.
+        </Box>
       </Stack>
-      </Container>
+    </Container>
   </div>
     // <div className="about-page-container">
     //   <h1 className="title">About Me</h1>
