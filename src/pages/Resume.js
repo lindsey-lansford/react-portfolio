2cecbbb43531f2b7e1bf4c5a0a469fbe33f36f19
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-12-01 13:39:50 -0700

    adding fetch function for dl resume pdf

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,23 @@
 import "../styles/resume.css";
+import React from "react";
 
 const Resume = () => {
+
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch('LL_Resume.pdf').then(response => {
+        response.blob().then(blob => {
+            // Creating new object of PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'LL_Resume.pdf';
+            alink.click();
+        })
+    })
+}
+
   return (
     <div className="resume-page-container">
       <h1 className="title">Resume</h1>
@@ -11,14 +28,17 @@ const Resume = () => {
           <p className="card-text">
             Checkout my resume by clicking the 'Download Here' button for a more indepth look into my work experience. Looking forward to chatting with you!
           </p>
-          <a
+          <button className="btn btn-primary" onClick={onButtonClick}>
+                    Download Here
+                </button>
+          {/* <a
             href="https://docs.google.com/document/d/1lyi5STj3BZu0Z-mMTJG6raPViqMHxkyrLzhgkDwnG1M/edit?usp=sharing"
             target="_blank"
             rel="noreferrer"
             className="btn btn-primary"
           >
             Download Here
-          </a>
+          </a> */}
         </div>
         <div className="card-footer text-muted" id="resume-footer"></div>
       </div>
