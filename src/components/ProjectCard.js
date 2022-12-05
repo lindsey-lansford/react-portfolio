1c12e2738b0c096d8162bcf516cbf2fd4f7352d1
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-12-05 15:51:00 -0700

    making sure this is defaulted

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,39 +1,39 @@
-import "../styles/projectCard.css";
-import * as React from 'react';
+// import "../styles/projectCard.css";
+// import * as React from 'react';
 
-const ProjectCard = (props) => {
-  return (
-    <div className="container-sm" id="portfolio-card">
-      <div className={`card text-center ${props.id}`}>
-        <div className="card-header">{props.title}</div>
-        <img src={props.image} alt={props.title} className="img-fluid rounded" />
-        <div className="card-body">
-          <div className="card-text">{props.description}</div>
-          <div className="card-body">
-            <a
-              href={props.repo}
-              target="_blank"
-              rel="noreferrer"
-              className="card-link"
-            >
-              {props.isBlog ? "Blog" : "GitHub Repo"}
-            </a>
+// const ProjectCard = (props) => {
+//   return (
+//     <div className="container-sm" id="portfolio-card">
+//       <div className={`card text-center ${props.id}`}>
+//         <div className="card-header">{props.title}</div>
+//         <img src={props.image} alt={props.title} className="img-fluid rounded" />
+//         <div className="card-body">
+//           <div className="card-text">{props.description}</div>
+//           <div className="card-body">
+//             <a
+//               href={props.repo}
+//               target="_blank"
+//               rel="noreferrer"
+//               className="card-link"
+//             >
+//               {props.isBlog ? "Blog" : "GitHub Repo"}
+//             </a>
             
-            {!props.isBlog && props.prod && (
-          <a
-          href={props.prod}
-          target="_blank"
-          rel="noreferrer"
-          className="card-link"
-              >
-                {"Production Link"}
-        </a>
-        )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+//             {!props.isBlog && props.prod && (
+//           <a
+//           href={props.prod}
+//           target="_blank"
+//           rel="noreferrer"
+//           className="card-link"
+//               >
+//                 {"Production Link"}
+//         </a>
+//         )}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ProjectCard;
+// export default ProjectCard;
