4a626daf882273a17f973bcb76540a8548b2f10f
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-12-07 18:24:50 -0700

    testing popovers in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,12 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import '../styles/footer.css';
 import { Popover, Typography, Link, Icon } from "@mui/material";
+import * as React from 'react';
+import { LinkedInIcon, GitHubIcon, EmailIcon } from "@mui/icons-material"
 
-import {LinkedInIcon, GitHubIcon, EmailIcon} from "@mui/icons-material"
 
 const Footer = () => {
-
-  // const [anchorEl, setAnchorEl] = React.useState(null);
-
-  // const handlePopoverOpen = (event) => {
-  //   setAnchorEl(event.currentTarget);
-  // };
-
-  // const handlePopoverClose = () => {
-  //   setAnchorEl(null);
-  // };
-
-  // const open = Boolean(anchorEl);
-
-
-  return (  
+    return (  
     <div className="footer-container">
       <footer>
         <ul>
@@ -61,6 +48,52 @@ const Footer = () => {
     </footer> 
     </div>
   );
+  
+  // const [anchorEl, setAnchorEl] = React.useState(null);
+
+  // const handlePopoverOpen = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+
+  // const handlePopoverClose = () => {
+  //   setAnchorEl(null);
+  // };
+
+  // const open = Boolean(anchorEl);
+
+  // return (
+  //   <div>
+  //     <Typography
+  //       aria-owns={open ? 'mouse-over-popover' : undefined}
+  //       aria-haspopup="true"
+  //       onMouseEnter={handlePopoverOpen}
+  //       onMouseLeave={handlePopoverClose}
+  //     >
+  //       Hover with a Popover.
+  //     </Typography>
+  //     <Popover
+  //       id="mouse-over-popover"
+  //       sx={{
+  //         pointerEvents: 'none',
+  //       }}
+  //       open={open}
+  //       anchorEl={anchorEl}
+  //       anchorOrigin={{
+  //         vertical: 'bottom',
+  //         horizontal: 'left',
+  //       }}
+  //       transformOrigin={{
+  //         vertical: 'top',
+  //         horizontal: 'left',
+  //       }}
+  //       onClose={handlePopoverClose}
+  //       disableRestoreFocus
+  //     >
+  //       <Typography sx={{ p: 1 }}>I use Popover.</Typography>
+  //     </Popover>
+  //   </div>
+  // );
 }
 
 export default Footer;
+
