527f4ad0bc0f0b46aae1f02827bd0ac518b35160
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-12-14 15:12:54 -0700

    adding custom date range/cta btn

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,17 +1,17 @@
-import GitHubCalendar from 'react-github-calendar';
-import { Row } from 'react-bootstrap';
+import GitHubCalendar from "react-github-calendar";
+import { Button } from "react-bootstrap";
+import "../styles/resume.css";
 
 const Github = () => {
-
-  const selectLastHalfYear = contributions => {
+  const selectLastHalfYear = (contributions) => {
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth();
     const shownMonths = 9;
-  
-    return contributions.filter(day => {
+
+    return contributions.filter((day) => {
       const date = new Date(day.date);
       const monthOfDay = date.getMonth();
-  
+
       return (
         date.getFullYear() === currentYear &&
         monthOfDay > currentMonth - shownMonths &&
@@ -21,15 +21,26 @@ const Github = () => {
   };
 
   return (
-    <Row>
+    <div className="mb-2 github-comp">
       <h1>My GitHub Activity</h1>
       <GitHubCalendar
-        username='lindsey-lansford'
-        color="#164A4A"
+        id="cal"
+        username="lindsey-lansford"
         transformData={selectLastHalfYear}
+        blockSize={10}
       />
-    </Row>
-
+      <div className="github-btn">
+        <Button
+          variant="dark"
+          href="https://github.com/lindsey-lansford"
+          target="_blank"
+          rel="noopener noreferrer"
+          size="sm"
+        >
+          View My Github
+        </Button>
+      </div>
+    </div>
   );
 };
 
