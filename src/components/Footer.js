795cd1a62a764671357be2773149c89c785ea22d
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-12-19 13:51:18 -0700

    removing old code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import '../styles/footer.css';
-// import { Popover, Typography, Link, Icon } from "@mui/material";
-
-// import { LinkedInIcon, GitHubIcon, EmailIcon } from "@mui/icons-material"
 
 
 const Footer = () => {
@@ -48,51 +45,7 @@ const Footer = () => {
     </footer> 
     </div>
   );
-  
-  // const [anchorEl, setAnchorEl] = React.useState(null);
 
-  // const handlePopoverOpen = (event) => {
-  //   setAnchorEl(event.currentTarget);
-  // };
-
-  // const handlePopoverClose = () => {
-  //   setAnchorEl(null);
-  // };
-
-  // const open = Boolean(anchorEl);
-
-  // return (
-  //   <div>
-  //     <Typography
-  //       aria-owns={open ? 'mouse-over-popover' : undefined}
-  //       aria-haspopup="true"
-  //       onMouseEnter={handlePopoverOpen}
-  //       onMouseLeave={handlePopoverClose}
-  //     >
-  //       Hover with a Popover.
-  //     </Typography>
-  //     <Popover
-  //       id="mouse-over-popover"
-  //       sx={{
-  //         pointerEvents: 'none',
-  //       }}
-  //       open={open}
-  //       anchorEl={anchorEl}
-  //       anchorOrigin={{
-  //         vertical: 'bottom',
-  //         horizontal: 'left',
-  //       }}
-  //       transformOrigin={{
-  //         vertical: 'top',
-  //         horizontal: 'left',
-  //       }}
-  //       onClose={handlePopoverClose}
-  //       disableRestoreFocus
-  //     >
-  //       <Typography sx={{ p: 1 }}>I use Popover.</Typography>
-  //     </Popover>
-  //   </div>
-  // );
 }
 
 export default Footer;
