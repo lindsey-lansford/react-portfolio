cc1afd17edcbfd0ef476a4f20e066f345f8f9c89
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-12-19 11:19:29 -0700

    editing my bio

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,39 +2,37 @@ import "../styles/about.css";
 import bioImage from "../images/profpic2.jpg";
 import { Container, Stack, Box } from '@mui/material';
 
-
 const About = () => {
-
-return (
-  <div className="about-page-container">
-    <h1 className="title">About Me</h1>
-    <Container>
-      <Stack
-        direction="row"
-        spacing={{ xs: 1, sm: 2, md: 4, lg: 5 }}
-        alignItems="center"
+  return (
+    <div className="about-page-container">
+      <h1 className="title">About Me</h1>
+      <Container>
+        <Stack
+          direction="row"
+          spacing={{ xs: 1, sm: 2, md: 4, lg: 5 }}
+          alignItems="center"
         >
-          <img className="profpic" src={bioImage} alt="profile"/>
-        <Box>
-        Hi y'all, my name is Lindsey and I'm a native Texan that has been living that Rocky Mountain High for 11 years and some change. I am based in Denver, CO and passionate about simple, clean web creation. I love photography, the great outdoors and breaking down challenges into smaller, more doable actions to reach the end goal.
-        </Box>
-      </Stack>
-      <Stack
-        display="flex"
-        direction="column"
-        spacing={{ xs: 1, sm: 2, md: 4 }}
-        alignItems="baseline"
-        className="lower-section"
-      >
-        <Box>
-        I am highly adaptable and love a challenge, big or small, I can do it all. My expertise lies in creating a vision, communicating the process to the client and executing within a given timeline.
-        </Box>
-        <Box>
-        HTML5 | CSS3 | JavaScript | Node.js | Express | ReactJS | SQL | MongoDB | Bootstrap | MaterialUI
-        </Box>
-      </Stack>
-    </Container>
-  </div>
+          <img className="profpic" src={bioImage} alt="profile" />
+          <Box>
+          Hi there! My name is Lindsey and I'm a full stack web developer based in Denver, CO. I have a passion for creating simple and clean websites, and love to photograph the great outdoors. I have 11 years of experience living in the Rocky Mountains, and have developed a strong ability to adapt to new challenges and break them down into smaller, more manageable tasks in order to achieve my goals.
+          </Box>
+        </Stack>
+        <Stack
+          display="flex"
+          direction="column"
+          spacing={{ xs: 1, sm: 2, md: 4 }}
+          alignItems="baseline"
+          className="lower-section"
+        >
+          <Box>
+          I pride myself on my ability to communicate effectively with clients and bring their vision to life. With my expertise in HTML5, CSS3, JavaScript, Node.js, Express, ReactJS, SQL, MongoDB, and Bootstrap, I am well-equipped to handle any project and deliver high-quality results within a given timeline.
+          </Box>
+          {/* <Box>
+            HTML5 | CSS3 | JavaScript | Node.js | Express | ReactJS | SQL | MongoDB | Bootstrap | MaterialUI
+          </Box> */}
+        </Stack>
+      </Container>
+    </div>
   );
 };
 
