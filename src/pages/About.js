8b9cec1747288485b9afef900a80908b0ec0561e
Lindsey Lansford <lindsey.lansford@gmail.com>
2023-01-27 14:16:06 -0700

    newpic! yay!

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import "../styles/about.css";
 import bioImage from "../images/profpic2.jpg";
+import bioImage2 from "../images/bioPic2.jpg";
 import { Container, Stack, Box } from '@mui/material';
 
 const About = () => {
@@ -12,7 +13,7 @@ const About = () => {
           spacing={{ xs: 1, sm: 2, md: 4, lg: 5 }}
           alignItems="center"
         >
-          <img className="profpic" src={bioImage} alt="profile" />
+          <img className="profpic" src={bioImage2} alt="profile" />
           <Box>
           Hi there! My name is Lindsey and I'm a full stack web developer based in Denver, CO. I have a passion for creating simple and clean websites, and love to photograph the great outdoors. I have 11 years of experience living in the Rocky Mountains, and have developed a strong ability to adapt to new challenges and break them down into smaller, more manageable tasks in order to achieve my goals.
           </Box>
