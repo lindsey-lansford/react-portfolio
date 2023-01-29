790648ea9ac894270cab19ea3af8afad0cb576bc
Lindsey Lansford <lindsey.lansford@gmail.com>
2023-01-29 11:47:46 -0700

    testing out animations

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,26 @@
+// import {bounce} from "react-animations";
+
+import styled, {keyframes} from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import '../styles/footer.css';
 
+
+// const Bounce = styled.div`
+//   animation:2s ${keyframes `${bounce}`} infinite`;
+// const rotate = keyframes`
+// from {
+//   transform: rotate(0deg);
+// }
+// to {
+//   transform: rotate(360deg);
+// }
+// `;
+
+// const Rotate = styled.div`
+//   animation: ${rotate} 2s linear hover;
+// `;
 
 const Footer = () => {
 
