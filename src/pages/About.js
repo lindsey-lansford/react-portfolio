2b88b3fb30e55d0cdab22de183ca1b5a06d9d842
Lindsey <lindsey.lansford@gmail.com>
2023-02-23 16:52:16 -0700

    replacing img tag

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import "../styles/about.css";
 // import bioImage from "../images/profpic2.jpg";
 import bioImage2 from "../images/bioPic2.jpg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Container, Stack, Box } from '@mui/material';
 //animation
 import styled, { keyframes } from "styled-components";
@@ -50,7 +51,7 @@ const About = () => {
           spacing={{ xs: 1, sm: 2, md: 4, lg: 5 }}
           alignItems="center"
         >
-          <img className="profpic" src={bioImage2} alt="profile" />
+          <LazyLoadImage className="profpic" src={bioImage2} alt="profile" />
           <Box>
           Hi there! My name is Lindsey and I'm a full stack web developer based in Denver, CO. I have a passion for creating simple and clean websites, and love to photograph the great outdoors. I have 11 years of experience living in the Rocky Mountains, and have developed a strong ability to adapt to new challenges and break them down into smaller, more manageable tasks in order to achieve my goals.
           </Box>
