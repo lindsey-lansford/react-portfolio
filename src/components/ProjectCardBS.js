58753fb8bde7569890455c6be27b2088cc80963a
Lindsey <lindsey.lansford@gmail.com>
2023-03-03 15:26:19 -0700

    adding it back to see if BS works better

---
+++ src/components/ProjectCardBS.js
@@ -0,0 +1,40 @@
+import "../styles/projectCard.css";
+
+const ProjectCard = (props) => {
+  return (
+    <div className="container-sm" id="portfolio-card">
+      <div className={`card text-center ${props.id}`}>
+        <div className="card-header">{props.title}</div>
+        <img src={props.image} alt={props.title} className="img-fluid rounded" />
+        <div className="card-body">
+          <div className="card-text">{props.description}</div>
+          <div className="card-body">
+            <a
+              href={props.repo}
+              target="_blank"
+              rel="noreferrer"
+              className="card-link"
+            >
+              {props.isDuel ? "Github" : "GitHub"}
+            </a>
+            
+            {!props.isDuel && props.prod && (
+          <a
+          href={props.prod}
+          target="_blank"
+          rel="noreferrer"
+          className="card-link"
+              >
+                {"Live"}
+        </a>
+        )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
+
+
