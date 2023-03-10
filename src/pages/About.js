d996b978fdfcb0da9aaf84bc1e26602d566dcd7c
Lindsey <lindsey.lansford@gmail.com>
2023-03-10 11:01:17 -0700

    removing unneeded code

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import "../styles/about.css";
-// import bioImage2 from "../images/bioPic2.jpg";
+
 import bioImageCom from "../images/bioPic2_compressed.jpg"
 // import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Container, Stack, Box } from "@mui/material";
