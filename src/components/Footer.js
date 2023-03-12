05c7baa8396c2687b538ae60f35a82bb08e37015
Lindsey <lindsey.lansford@gmail.com>
2023-03-11 17:02:07 -0700

    formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,7 +26,7 @@ const Footer = () => {
   let date = new Date();
   let year = date.getFullYear();
 
-    return (  
+  return (
     <div className="footer-container">
       <footer>
         <ul>
@@ -34,44 +34,44 @@ const Footer = () => {
             <a href="https://www.linkedin.com/in/lindsey-lansford/"
               target="_blank"
               rel="noreferrer">
-                <Tada>
+              <Tada>
                 <FontAwesomeIcon
                   icon={faLinkedin}
                   className="social-icon"
-                  />
-                  </Tada>
-              </a>
+                />
+              </Tada>
+            </a>
           </li>
           <li>
             <a
               href="https://github.com/lindsey-lansford"
               target="_blank"
               rel="noopener noreferrer">
-                <Tada>
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="social-icon"
-                  />
-                  </Tada>
+              <Tada>
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="social-icon"
+                />
+              </Tada>
             </a>
           </li>
           <li>
-              <a href="mailto:lindsey.lansford@gmail.com">
-                <Tada>
-              <FontAwesomeIcon
-                icon={faEnvelope}
-                className="social-icon"
-                  />
-                  </Tada>
+            <a
+              href="mailto:lindsey.lansford@gmail.com">
+              <Tada>
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="social-icon"
+                />
+              </Tada>
             </a>
           </li>
         </ul>
         <span className="footer-name">&copy; {year} 🧿Lindsey Lansford | Software Developer</span>
-    </footer> 
+      </footer>
     </div>
   );
-
-}
+};
 
 export default Footer;
 
